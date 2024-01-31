f4f502a4d89c8988718ff2ba72b4f9d0bfd888b9
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-01-31 21:06:31 +0100

    ray docs

--- includes/ray.cuh
+++ includes/ray.cuh
@@ -4,18 +4,46 @@
 # include "vec3.cuh"
 
 
+/**
+ * @brief Class representing rays
+ * 
+ * A class that represents rays as a tuple of origin and direction
+ * as vec3 and some utilities functions
+*/
 class ray
 {
 	public:
 		vec3	orig;
 		vec3	dir;
 
+		/**
+		 * @brief Empty Costructor
+		*/
 		__device__ ray() {}
+
+		/**
+		 * @brief Standard Costructor
+		 * @param origin the origin point of the ray
+		 * @param direction the direction of the ray
+		*/
 		__device__ ray(const vec3 &origin, const vec3 &direction): orig(origin), dir(direction) {}
 		
+		/**
+		 * @brief A getter for origin
+		 * @return the origin of the ray
+		*/
 		__device__ vec3	origin() const { return (this->orig); }
+		
+		/**
+		 * @brief A getter for direction
+		 * @return the direction of the ray
+		*/
 		__device__ vec3	direction() const { return (this->dir); }
 
+		/**
+		 * @brief A scalar product of the ray
+		 * @return the product of the scalar and the ray 
+		*/
 		__device__ vec3	at(const double t) const { return (this->orig + this->dir * t); }
 };
 
