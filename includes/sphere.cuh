ce01738e9e9948635657b2886ff9288892c370c8
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-01-31 21:06:42 +0100

    sphere docs

--- includes/sphere.cuh
+++ includes/sphere.cuh
@@ -5,20 +5,45 @@
 # include "hittable.cuh"
 
 
+/**
+ * @brief A sphere object derived by hittable
+ * 
+ * This class represents a sphere with its core functions
+*/
 class sphere: public hittable
 {
 	public:
 		vec3		center;
 		float		radius;
 		material	*mat;
-		
+
+		/**
+		 * @brief Empty Constructor
+		*/
 		__device__ sphere() {}
+
+		/**
+		 * @brief Standard Constructor
+		 * @param c the center of the sphere
+		 * @param r the radius of the sphere
+		 * @param m the material of the sphere
+		*/
 		__device__ sphere(vec3 c, float r, material *m): center(c), radius(r), mat(m) {};
 
 		__device__ virtual bool	hit(const ray &r, float t_min, float t_max, t_hit_record &rec) const override;
 };
 
 
+/**
+ * @brief A function to check if hitted
+ * @param r the ray to analyze
+ * @param t_min the minimum range span to hit
+ * @param t_max the maximum range span to hit
+ * @param rec the output structure filled with hit record data
+ * @return the boolean value of true if hitten or false if not
+ * 
+ * This function is called to check if a certain ray is hitting the current sphere
+*/
 __device__ bool	sphere::hit(const ray &r, float t_min, float t_max, t_hit_record &rec) const
 {
 	vec3	oc;
@@ -56,6 +81,17 @@ __device__ bool	sphere::hit(const ray &r, float t_min, float t_max, t_hit_record
 }
 
 
+/**
+ * @brief An optimized version of sphere->hit
+ * @param r the ray to analyze
+ * @param t_min the minimum range span to hit
+ * @param t_max the maximum range span to hit
+ * @param rec the output structure filled with hit record data
+ * @return the boolean value of true if hitten or false if not
+ * 
+ * An optimized version of sphere->hit which skips a lot of vtable
+ * fetches and som function calls to gain performances at runtime
+*/
 __device__ bool	O_hit(sphere *s, const ray &r, float t_min, float t_max, t_hit_record &rec)
 {
 	vec3	oc;
@@ -66,11 +102,8 @@ __device__ bool	O_hit(sphere *s, const ray &r, float t_min, float t_max, t_hit_r
 	float	sqrtd;
 	float	root;
 
-	//oc = r.origin() - s->center;
 	oc = r.orig - s->center;
-	//a = r.direction().length_squared();
 	a = r.dir.length_squared();
-	//half_b = dot(oc, r.direction());
 	half_b = dot(oc, r.dir);
 	c = oc.length_squared() - s->radius * s->radius;
 	
@@ -88,7 +121,6 @@ __device__ bool	O_hit(sphere *s, const ray &r, float t_min, float t_max, t_hit_r
 	}
 
 	rec.t = root;
-	//rec.p = r.at(rec.t);
 	rec.p = r.orig + r.dir * rec.t;
 	rec.normal = (rec.p - s->center) / s->radius;
 	rec.mat = s->mat;
