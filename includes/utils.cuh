a776b5a2a85b666158d66a9485174d4b2aebb362
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-01-31 21:06:58 +0100

    utils docs

--- includes/utils.cuh
+++ includes/utils.cuh
@@ -8,6 +8,17 @@
 
 // https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__TYPES.html
 # define CHECK(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+/**
+ * @brief A function to check for errors
+ * @param res the cudaError result of the checked function
+ * @param func the function that was called
+ * @param file the string of the origin file
+ * @param line the number of line where the error occurred
+ * 
+ * This function checks the result of a called function and throws an
+ * error waring with detailed info and stops the execution
+*/
 void check_cuda(cudaError_t res, const char *func, const char *file, const int line)
 {
 	if (!res)
